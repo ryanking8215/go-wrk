73a077e2fb09b6997d6d4836457fc4fb86f14549
Jin Zhihua <zh.jin@catontechnology.com>
2022-10-08 14:41:14 +0800

    fix script

--- scripts/post.js
+++ scripts/post.js
@@ -1,8 +1,5 @@
 "use strict";
 
-//wrk.method = "POST";
+wrk.method = "POST";
 wrk.body   = "foo=bar&baz=quux";
 wrk.header["Content-Type"] = "application/x-www-form-urlencoded";
-
-console.log(wrk)
-console.log(wrk.id)
